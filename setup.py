65cdb43322bb263a2820cf5791623c24d5b7302d
maayanorner <mayanorner@gmail.com>
2024-10-01 23:37:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='iahlt_coref_he',
     version='0.1.0',
-    packages=['iahlt_coref_he'],  # Replace with your actual package directory
+    packages=['iahlt_coref_he'],
     install_requires=[
         'ufal.udpipe==1.3.1.1',
         'fastcoref==2.1.6',
